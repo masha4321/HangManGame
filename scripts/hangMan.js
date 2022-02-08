cb88adc890dbe354df59b5b53c2fc252d4f9c4f2
DeePee86 <91700375+DeePee86@users.noreply.github.com>
2022-02-07 20:56:01 -0500

    Update hangMan.js

--- scripts/hangMan.js
+++ scripts/hangMan.js
@@ -35,3 +35,25 @@ for (let i = 0; i < alphabet.length; i++) {
 }
 //End of Alphabet
 }
+
+// play again + won/lost - some code is commented as i was not sure of 
+// the elements names we will use
+let gameHm = document.getElementById("insertElemName");
+
+//create button to play again ( gamefunction will reset game values)
+let btnREPLAY = document.createElement("button");
+btnREPLAY.innerText = "Play Again";
+btnREPLAY.style.visibility = "hidden";
+btnREPLAY.onclick = //gamefunction//;
+gameHm.appendChild(btnREPLAY);
+
+// won/lost - will have to include it in the game script
+let gameScore = document.getElementById("scoreElem")
+if (tries == maxtries) {
+    gameScore.innerHTML = "Game Over. You have run out of tries, please play again!";
+    btnREPLAY.style.visibility = "visible";   
+}
+else if //(guessWord == generatedWord){ // we can also use if guessWord != blank
+    gameScore.innerHTML = "Congratulations, you have won!"
+    btnREPLAY.style.visibility = "visible";
+}
