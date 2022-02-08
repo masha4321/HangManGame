40d84516f26b1efb5b8cbad838d840f419d44e5e
glenpham <43647463+glenpham@users.noreply.github.com>
2022-02-08 05:29:27 -0500

    added the lives functions, filled in the word to guess , made the alphabet clickable

--- scripts/hangMan.js
+++ scripts/hangMan.js
@@ -1,3 +1,4 @@
+let nbOfLives = 10;
 let randomNumber = 0;
 let wordChosen = "";
 let hint = "";
@@ -7,6 +8,7 @@ $(document).ready(function () {
   $(".playBtn").click(function () {
     $(".center").fadeOut();
     $(".hintBox").removeAttr("hidden");
+    $("#livesText").removeAttr("hidden");
     playGame();
   });
 });
@@ -14,40 +16,87 @@ $(document).ready(function () {
 
 function playGame() {
 
-let displayBox = document.getElementById("imgBox");
-displayBox.style.display="block";
+  // NB of lives - GOES IN SWETUP
+  let gameScore = document.getElementById("livesText").innerHTML = "You have " + nbOfLives + " left";
 
-//Word to Guess
-let wordBox = document.getElementById("boxes")
-wordBox.style.padding = "50px";
-for (let i = 0; i<10; i++) {
-  let li=document.createElement("li");
-  li.style.backgroundColor="grey";
-  li.appendChild(document.createTextNode("_"));
-  wordBox.append(li);
-}
+  let displayBox = document.getElementById("imgBox");
+  displayBox.style.display="block";
+
+  //Word to Guess
+  let wordBox = document.getElementById("boxes")
+  wordBox.style.padding = "50px";
+  for (let i = 0; i<10; i++) {
+    let li=document.createElement("li");
+    li.style.backgroundColor="grey";
+    li.appendChild(document.createTextNode("_"));
+    wordBox.append(li);
+  }
 
 
-//Alphabet
-let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let ul = document.getElementById("letters");
+  //Alphabet
+  let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let ul = document.getElementById("letters");
 
-for (let i = 0; i < alphabet.length; i++) {
-  let letter = alphabet[i];
-  let li = document.createElement("li");
-  li.appendChild(document.createTextNode(letter));
-  ul.appendChild(li);
-}
-//End of Alphabet
+  for (let i = 0; i < alphabet.length; i++) {
+    let letter = alphabet[i];
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(letter));
+    ul.appendChild(li);
+  }
+  //End of Alphabet
 
-randomWordGenerator();
-hintGenerator();
-document.getElementById("hintBtn").addEventListener("click",displayHint);
+  function checkLetter(){
+    let chosenLetter = this.innerHTML;
+    let wordHolder = wordBox.querySelectorAll("li");
+    gameScore = document.getElementById("livesText");
+    let letterFound = false;
+
+    for( i = 0; i < wordChosen.length; i++ ){
+      if( chosenLetter == wordChosen[i] ){
+        wordHolder[i].innerHTML = chosenLetter;
+        letterFound = true;
+      }
+    }
+
+    if(!letterFound){
+      nbOfLives--;
+    }
+    gameScore.innerHTML = "You have " + nbOfLives + " left";
+    checkLives();
+  }
+
+  randomWordGenerator();
+  hintGenerator();
+
+  let lettersDisplayed = ul.querySelectorAll("li");
+  for (let i = 0; i < alphabet.length; i++){
+    lettersDisplayed[i].addEventListener("click", checkLetter);
+  }
+  document.getElementById("hintBtn").addEventListener("click",displayHint);
 }
 
 /*Glen's Section Start */
+function checkLives(){
+  let winCondition = true;
+
+  for( i = 0; i < wordChosen.length; i++ ){
+    if( document.getElementById("boxes").querySelectorAll("li")[i].innerHTML != wordChosen[i]){
+      winCondition = false
+    }
+  }
+
+  if (nbOfLives == 0) {
+    document.getElementById("livesText").innerHTML = "Game Over. You have run out of lives, please play again!";
+    btnREPLAY.style.visibility = "visible";   
+  }
+  if (nbOfLives != 0 && winCondition){
+    document.getElementById("livesText").innerHTML = "Congratulations, you have won!"
+    btnREPLAY.style.visibility = "visible";
+  }
+}
+
 function randomWordGenerator() {
-  let wordDatabase = ["automation", "Strawberry", "Friendship", "Everything", "Appreciate", "Ubiquitous", "Motivation", "Vaccinated", "Obediently", "Earthbound"]
+  let wordDatabase = ["automation", "strawberry", "friendship", "everything", "appreciate", "ubiquitous", "motivation", "vaccinated", "obediently", "earthbound"]
   randomNumber = Math.floor(Math.random() * 10) + 1;
   wordChosen = wordDatabase[randomNumber-1]
   console.log(wordChosen)
@@ -88,22 +137,25 @@ function hintGenerator() {
   }
   
 }
+
 function displayHint(){
   document.getElementById("hintText").innerHTML += hint;
   document.getElementById("hintBtn").disabled = true;
 }
 /* Glen's Section End*/
 
+
+
 // play again + won/lost - some code is commented as i was not sure of 
 // the elements names we will use
-let gameHm = document.getElementById("insertElemName");
+let gameHm = document.getElementById("buttons");
 
 //create button to play again ( gamefunction will reset game values)
 let btnREPLAY = document.createElement("button");
+btnREPLAY.id = "btnReplay";
 btnREPLAY.innerText = "Play Again";
 btnREPLAY.style.visibility = "hidden";
 btnREPLAY.onclick = //gamefunction//;
 gameHm.appendChild(btnREPLAY);
-
 
 
