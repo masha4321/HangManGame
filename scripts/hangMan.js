8f9462ee7449604880f329df6db8dd2d49a0aaba
masha4321 <masharik09@mail.ru>
2022-02-07 19:12:12 -0500

    Added letters

--- scripts/hangMan.js
+++ scripts/hangMan.js
@@ -1,7 +1,46 @@
 //Play Btn
-$(document).ready(function(){
-    $(".playBtn").click(function(){
-      $(".center").fadeOut();
-    });
+$(document).ready(function () {
+  $(".playBtn").click(function () {
+    $(".center").fadeOut();
   });
+});
 //End of Play Btn
+
+//Alphabet
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let ul = document.getElementById("letters");
+
+for (let i = 0; i < alphabet.length; i++) {
+  let letter = alphabet[i];
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(letter));
+  ul.appendChild(li);
+}
+//End of Alphabet
