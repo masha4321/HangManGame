72e3d3a35c9cbb5bb721b3694666f03a75ef7884
davidkrushnisky <91700482+davidkrushnisky@users.noreply.github.com>
2022-02-07 20:05:22 -0500

    Word to Guess

--- scripts/hangMan.js
+++ scripts/hangMan.js
@@ -2,39 +2,26 @@
 $(document).ready(function () {
   $(".playBtn").click(function () {
     $(".center").fadeOut();
+    playGame();
   });
 });
 //End of Play Btn
 
+function playGame() {
+
+//Word to Guess
+let wordBox = document.getElementById("boxes")
+wordBox.style.padding = "50px";
+for (let i = 0; i<7; i++) {
+  let li=document.createElement("li");
+  li.style.backgroundColor="grey";
+  li.appendChild(document.createTextNode("_"));
+  wordBox.append(li);
+}
+
+
 //Alphabet
-let alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let ul = document.getElementById("letters");
 
 for (let i = 0; i < alphabet.length; i++) {
@@ -44,3 +31,4 @@ for (let i = 0; i < alphabet.length; i++) {
   ul.appendChild(li);
 }
 //End of Alphabet
+}
