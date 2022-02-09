ca2978ea1b206203f8cdcf3828a72cfc43448f9a
DeePee86 <91700375+DeePee86@users.noreply.github.com>
2022-02-09 04:43:35 -0500

    Update hangMan.js

--- scripts/hangMan.js
+++ scripts/hangMan.js
@@ -193,7 +193,7 @@ function displayHint() {
   document.getElementById("hintBtn").disabled = true;
 }
 
-// reset game values(not complete, working on lettersDisplayed and alphabet as they repeat when "play again" is clicked)
+// reset game values(not complete, working on alphabet and the guess word as they repeat when "play again" is clicked)
 function reset(){
   nbOfLives = 10;
   randomNumber = 0;
